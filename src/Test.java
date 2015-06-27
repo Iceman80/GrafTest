6fa347999981059b40c4ca9ac802af73424c92e7
Serhiy Yakovlev <ysa.sergey@gmail.com>
2015-06-27 14:53:24 +0300

    1

--- src/Test.java
+++ src/Test.java
@@ -24,25 +24,17 @@ public class Test extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - Yakovlev Serhiy
         panel1 = new JPanel();
         button1 = new JButton();
         label1 = new JLabel();
 
         //======== this ========
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         Container contentPane = getContentPane();
 
         //======== panel1 ========
         {
             panel1.setBackground(Color.white);
-
-            // JFormDesigner evaluation mark
-            panel1.setBorder(new javax.swing.border.CompoundBorder(
-                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                    java.awt.Color.red), panel1.getBorder())); panel1.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
 
             //---- button1 ----
             button1.setText("\u0416\u043c\u0438 \u0437\u0434\u0435\u0441\u044c");
@@ -95,7 +87,6 @@ public class Test extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Yakovlev Serhiy
     private JPanel panel1;
     private JButton button1;
     private JLabel label1;
